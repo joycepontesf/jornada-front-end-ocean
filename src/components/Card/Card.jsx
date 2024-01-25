ddeead3e17a0972aafd08cdccf683ff29a241afe
Joyce Pontes <60980108+joycepontesf@users.noreply.github.com>
2024-01-25 15:04:26 +0000

    Alteração do conteúdo da página, inclusão da folha de estilo e reorganização de componentes

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+function Card() {
+    return (
+        <>
+          
+          <div className='cardDancerMorty'>
+            <h1>Dancer Morty</h1>
+            <img src="https://rickandmortyapi.com/api/character/avatar/475.jpeg"/>
+            </div>
+            <br></br>
+            <div className='cardDannyPublitz'>
+            <h1>Danny Publitz</h1>
+            <img src="https://rickandmortyapi.com/api/character/avatar/526.jpeg"/>
+          </div>
+        </>
+    )
+}
+
+export default Card
