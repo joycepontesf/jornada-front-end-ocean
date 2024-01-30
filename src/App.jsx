420825e70087553ba7baf9f634a5901809eeba9f
Joyce Pontes <60980108+joycepontesf@users.noreply.github.com>
2024-01-30 14:11:34 +0000

    Estilização da lista de cards

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,34 @@ function App() {
     created: 'Criação: 04-11-2017'
 
   }
+  const item3 = {
+    name: 'Johnny Depp',
+    image: 'https://rickandmortyapi.com/api/character/avatar/183.jpeg',
+    status: 'Status: Vivo',
+    species: 'Espécie: Humana',
+    origin: 'Origem: Terra (C-500A)',
+    created: 'Criação: 29-12-2017'
+
+  }
+
+  const item4 = {
+    name: 'Jerry Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+    status: 'Status: Vivo',
+    species: 'Espécie: Humana',
+    origin: 'Origem: Terra',
+    created: 'Criação: 04-11-2017'
+
+  }
 
   return (
     <>
+    <div className='cards'> 
       <Card item={item1} />
       <Card item={item2} />
+      <Card item={item3} />
+      <Card item={item4} />
+    </div>
     </>
   )
 }
