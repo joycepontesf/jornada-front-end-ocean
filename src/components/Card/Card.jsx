3cbe19f2fcd671531d88746a5c323c6f0daa95b0
Joyce Pontes <60980108+joycepontesf@users.noreply.github.com>
2024-01-30 12:49:49 +0000

    Criação e estilização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,15 @@ function Card() {
     return (
         <>
           
-          <div className='cardDancerMorty'>
-            <h1>Dancer Morty</h1>
-            <img src="https://rickandmortyapi.com/api/character/avatar/475.jpeg"/>
-            </div>
-            <br></br>
-            <div className='cardDannyPublitz'>
-            <h1>Danny Publitz</h1>
-            <img src="https://rickandmortyapi.com/api/character/avatar/526.jpeg"/>
+          <div className='card'>
+            <h2>Morty Smith</h2>
+          <div className="tags">  
+            <div className="tag">Status: Vivo</div>
+            <div className="tag">Espécie: Humana</div>
+            <div className="tag">Origem: Terra</div>
+            <div className="tag">Criação: 30-12-2017</div>
+           </div> 
+            <img src="https://rickandmortyapi.com/api/character/avatar/232.jpeg"/>
           </div>
         </>
     )
