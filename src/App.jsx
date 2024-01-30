31020f8dee36cd4465b124f0f53f29321ecaa561
Joyce Pontes <60980108+joycepontesf@users.noreply.github.com>
2024-01-30 13:45:47 +0000

    Otimização dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,29 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/232.jpeg',
+    status: 'Status: Vivo',
+    species: 'Espécie: Humana',
+    origin: 'Origem: Terra',
+    created: 'Criação: 30-12-2017'
+
+  }
+  const item2 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    status: 'Status: Vivo',
+    species: 'Espécie: Humana',
+    origin: 'Origem: Terra (C-137)',
+    created: 'Criação: 04-11-2017'
+
+  }
 
   return (
     <>
-      <Card/>
+      <Card item={item1} />
+      <Card item={item2} />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,27 @@
-function Card() {
-    return (
-        <>
-          
-          <div className='card'>
-            <h2>Morty Smith</h2>
-          <div className="tags">  
-            <div className="tag">Status: Vivo</div>
-            <div className="tag">Espécie: Humana</div>
-            <div className="tag">Origem: Terra</div>
-            <div className="tag">Criação: 30-12-2017</div>
-           </div> 
-            <img src="https://rickandmortyapi.com/api/character/avatar/232.jpeg"/>
-          </div>
-        </>
-    )
+function Card(props) {
+  //console.log(props)
+
+  const item = props.item
+
+  console.log(item)
+
+  return (
+    <>
+
+      <div className='card'>
+        <h2>{item.name}</h2>
+        
+        <div className="tags">
+          <div className="tag">{item.status}</div>
+          <div className="tag">{item.species}</div>
+          <div className="tag">{item.origin}</div>
+          <div className="tag">{item.created}</div>
+        </div>
+        
+        <img src={item.image}/>
+      </div>
+    </>
+  )
 }
 
 export default Card
